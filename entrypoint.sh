d01bee76893696316d6d9f94f7828a0f716f63ac
zanderhavgaard <zander@havgaard.dk>
2021-01-27 11:02:09 +0100

    udpated entrypoint.sh script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-# TODO change to run on orchestration server
-
 docker run --rm -it \
-    -v /home/zander/eficode/radon/cloudstash-characterization/EC2/secrets/.aws:/home/alpine/.aws:ro \
-    -v /home/zander/eficode/radon/cloudstash/serverless:/home/alpine/serverless \
-    -v /home/zander/eficode/radon/cloudstash-characterization:/home/alpine/cloudstash-characterization:ro \
-    cctest:latest \
+    -v /home/alpine/cloudstash-characterization/EC2/secrets/.aws:/home/alpine/.aws:ro \
+    -v /home/alpine/cloudstash/serverless:/home/alpine/serverless \
+    docker.pkg.github.com/radon-h2020/cloudstash-characterization/cloudstash-benchmarker:main \
     bash
