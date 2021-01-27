2b396d6870546ea03c0f2066c0edf1fa5d45a48c
zanderhavgaard <zander@havgaard.dk>
2021-01-27 12:00:16 +0100

    fixed paths in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 docker run --rm -it \
-    -v /home/alpine/cloudstash-characterization/EC2/secrets/.aws:/home/alpine/.aws:ro \
-    -v /home/alpine/cloudstash/serverless:/home/alpine/serverless \
+    -v /home/ubuntu/.aws:/home/alpine/.aws:ro \
+    -v /home/ubuntu/cloudstash/serverless:/home/alpine/serverless \
     docker.pkg.github.com/radon-h2020/cloudstash-characterization/cloudstash-benchmarker:main \
     bash
